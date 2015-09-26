b1f729436188151bf040614bd62dd62fd9d35146
Christopher Su <christophersu9@gmail.com>
2015-09-25 19:27:33 -0700

    example

--- example.py
+++ example.py
@@ -3,3 +3,7 @@ from secrets import ACCOUNT_SID, AUTH_TOKEN
 
 sender = Twilio(ACCOUNT_SID, AUTH_TOKEN)
 batcher = BatchSMS(sender)
+
+body = 'hello, this is my message'
+to_numbers = ['0123456789', '9876543210']
+batcher.send_sms(body, to_numbers)
