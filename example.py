eca1273aa8e3e764d439e80b36ca75aff68316fc
Christopher Su <christophersu9@gmail.com>
2015-10-04 19:35:10 -0700

    example

--- example.py
+++ example.py
@@ -1,12 +1,12 @@
-from batch_sms import BatchSMS, TwilioSender
+from batch_sms import AssociatedBatchSender, TwilioSender
 from secrets import TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, test_from_num, test_to_num
 
 def send_message_callback(payload):
     print payload
 
 sender = TwilioSender(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
-batcher = BatchSMS(sender)
+batch_sender = AssociatedBatchSender(sender)
 
 body = 'hello, this is my message'
 numbers = {test_from_num: [test_to_num]}
-batcher.send_sms(body, numbers, callback=send_message_callback)
+batch_sender.send_sms(body, numbers, callback=send_message_callback)
