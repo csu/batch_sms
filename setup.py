7b3c96fa4ca1df2223426fc25780b03ed3d222ab
Christopher Su <christophersu9@gmail.com>
2015-10-26 21:40:14 -0700

    wat

--- setup.py
+++ setup.py
@@ -16,18 +16,7 @@ setup(
     url='https://github.com/csu/batch_sms',
     packages=['batch_sms'],
     install_requires=[
-        "alembic==0.8.2",
         "dataset==0.6.1",
-        "httplib2==0.9.2",
-        "Mako==1.0.2",
-        "MarkupSafe==0.23",
-        "normality==0.2.4",
-        "python-editor==0.4",
-        "pytz==2015.6",
-        "PyYAML==3.11",
-        "six==1.10.0",
-        "SQLAlchemy==1.0.8",
-        "twilio==4.6.0",
-        "wheel==0.24.0"
+        "twilio==4.6.0"
     ]
 )
